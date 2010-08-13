2b645e6481e185c5f626da578f8cb1d8d6fdfb9b
alzeih <alzeih@gmail.com>
2010-08-13 17:56:39 +1200

    Simple example launch script

---
+++ launch.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+mono bin/Debug/bot.exe --server "irc.example.com" --channel "#example" --bitly-username "bitlyapidemo" --bitly-key "R_0da49e0a9118ff35f52f629d2d71bf07" --authorized-nicks "nick1,nick2" 
